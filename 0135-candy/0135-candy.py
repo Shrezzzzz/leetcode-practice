1df9dfa8d6dd5254acb08df2388b075946bc6358
Shreya Chowdhury <shreyachowdhury973@gmail.com>
2026-08-04 13:18:00 +0530

    Time: 16 ms (80.89%), Space: 14.6 MB (52.47%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n
+        
+        # Left-to-right pass: enforce "higher rating than LEFT neighbor gets more candy"
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+        
+        # Right-to-left pass: enforce "higher rating than RIGHT neighbor gets more candy"
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+        
+        return sum(candies)
