33bae6d726a07573cafbb49d782bea08299ac0b5
Shreya Chowdhury <shreyachowdhury973@gmail.com>
2026-09-21 22:52:31 +0530

    Time: 849 ms (60.65%), Space: 31.3 MB (41.9%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        seen = set()
+        repeated = set()
+        
+        for i in range(len(s) - 9):
+            substring = s[i:i + 10]
+            
+            if substring in seen:
+                repeated.add(substring)
+            else:
+                seen.add(substring)
+        
+        return list(repeated)
